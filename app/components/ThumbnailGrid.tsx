4b5c953e5f5b594913c7938b0aa33e95b1c585d1
Eric Hovda <ehovda321@gmail.com>
2024-11-02 17:15:51 -0500

    Removed videos... trial4

--- app/components/ThumbnailGrid.tsx
+++ app/components/ThumbnailGrid.tsx
@@ -3,9 +3,6 @@ import { useEffect, useState } from "react";
 export default function ThumbnailGrid({ onThumbnailClick }: any) {
     return <div className="@container">
         <div className="grid grid-cols-1 @lg:grid-cols-2 @xl:grid-cols-3 @3xl:grid-cols-4 gap-6">
-            {
-                thumbnails
-            }
         </div>
     </div>
 }
